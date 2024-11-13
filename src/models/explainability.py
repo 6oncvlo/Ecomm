6eb22c45371142af6aa2990e8374136501ae0f77
6oncvlo <gonprt06@gmail.com>
2024-11-13 21:59:24 +0000

    feature importance module

---
+++ src/models/explainability.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+def feature_importance(model, dataframe: pd.DataFrame, model_columns: list)->pd.DataFrame:
+    
+    dataframe = dataframe.loc[:, model_columns]
+    # Initialize array to store feature contributions
+    feature_importances = np.zeros(dataframe.shape[1])
+
+    # Access 'estimators_' safely
+    estimators = model.get_model_attribute("estimators_")
+    for tree in estimators:
+        tree_features = tree.tree_.feature
+        # Count how many times each feature is used across all splits in this tree
+        for feature in range(dataframe.shape[1]):
+            # Sum path lengths for nodes where this feature is used to split
+            feature_importances[feature] += np.sum(tree_features == feature)
+
+    # Normalize to get relative feature contributions
+    feature_importances /= feature_importances.sum()
+    feature_contributions = pd.Series(feature_importances, index=model_columns)
+    return pd.DataFrame(feature_contributions, columns=['weight']).sort_values(by='weight', ascending=False)
