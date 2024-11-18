7d910ce836af7f1ed5c03f5b19c7f14b7a980792
6oncvlo <gonprt06@gmail.com>
2024-11-18 13:30:56 +0000

    model explainability class

--- src/models/explainability.py
+++ src/models/explainability.py
@@ -6,6 +6,7 @@ class ModelExplainability:
     def __init__(self, model, data: pd.DataFrame):
         self.model = model
         self.data = data
+        self.shap_values = self.ShapValues(self)
 
     def feature_importance(self):
         # Initialize array to store feature contributions
@@ -24,3 +25,46 @@ class ModelExplainability:
         feature_importances /= feature_importances.sum()
         feature_contributions = pd.Series(feature_importances, index=self.data.columns)
         return pd.DataFrame(feature_contributions, columns=['weight']).sort_values(by='weight', ascending=False)
+    
+    class ShapValues:
+        def __init__(self, parent):
+            self.parent = parent
+            self.explainer = None  # Initialize explainer as None
+            self._shap_values = None  # Private attribute for SHAP values
+    
+        @property
+        def shap_values(self):
+            # Compute SHAP values only when accessed
+            if self._shap_values is None:
+                self.explainer = shap.TreeExplainer(self.parent.model.model)
+                self._shap_values = self.explainer.shap_values(self.parent.data)
+            return self._shap_values
+    
+        def plot(self, method: str, ind=None, interaction_index=None, instance_index=None):
+            # Ensure SHAP values are computed before plotting
+            _ = self.shap_values
+            
+            if method == 'global':
+                # Global summary plot to view the overall feature importance
+                shap.summary_plot(
+                    shap_values = self.shap_values
+                    , features = self.parent.data
+                    , max_display = 20
+                    , plot_type = "bar"
+                    , plot_size = (10,10)
+                    )
+            elif method == 'dependence':
+                # Use a SHAP dependence plot for specific features
+                shap.dependence_plot(
+                    ind = ind
+                    , shap_values = self.shap_values
+                    , features = self.parent.data
+                    , interaction_index = interaction_index
+                    )
+            elif method == 'instance':
+                # For a specific instance, view SHAP values and plot them
+                shap.force_plot(
+                    base_value  = self.explainer.expected_value
+                    , shap_values = self.shap_values[instance_index]
+                    , features = self.parent.data.iloc[instance_index]
+                    )
