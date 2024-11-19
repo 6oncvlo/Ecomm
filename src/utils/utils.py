31c37c924791f74a040d9f17efcadc408658c51c
6oncvlo <gonprt06@gmail.com>
2024-11-19 18:08:47 +0000

    sample and k_distance

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,8 +1,10 @@
 import pandas as pd
-
+from sklearn.model_selection import StratifiedShuffleSplit
+import faiss
+import numpy as np
 
 def downcast_cols(dataframe: pd.DataFrame):
-    """ downcast floats & integers to the smallest possible type """
+    """Downcast floats & integers to the smallest possible type."""
 
     fcols = dataframe.select_dtypes('float').columns
     icols = dataframe.select_dtypes('integer').columns
@@ -11,3 +13,26 @@ def downcast_cols(dataframe: pd.DataFrame):
     dataframe[icols] = dataframe[icols].apply(pd.to_numeric, downcast='integer')
         
     return dataframe
+
+def sampling(sampling_method: str, n_splits: int, sample_size: float, dataframe: pd.DataFrame):
+    """Perform stratified or random sampling. Last column is label for stratified sampling."""
+    
+    if sampling_method == 'stratified_sampling':
+        # Perform stratified sampling
+        splitter = StratifiedShuffleSplit(n_splits=n_splits, test_size=sample_size, random_state=42)
+        datasets = [
+            dataframe.iloc[test_index,:-1]
+            for _, test_index in splitter.split(X=dataframe.iloc[:,:-1], y=dataframe.iloc[:,-1])
+        ]
+    else:
+        # Perform random sampling
+        datasets = [dataframe.iloc[:,:-1].sample(frac=sample_size, random_state=42 + i) for i in range(n_splits)]
+    return datasets
+
+def compute_k_distances(data, n_neighbors):
+    """Compute the k-distance graph data for the given dataset."""
+    
+    index = faiss.IndexFlatL2(data.shape[1])
+    index.add(data)
+    distances, _ = index.search(data, n_neighbors)
+    return np.sort(distances[:, -1])
