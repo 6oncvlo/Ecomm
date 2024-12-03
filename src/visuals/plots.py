c875937719c933defdd92241d9a564571b7389e9
6oncvlo <gonprt06@gmail.com>
2024-12-03 16:05:45 +0000

    input is numpy array in k-plot

--- src/visuals/plots.py
+++ src/visuals/plots.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -30,18 +31,18 @@ def kde_group(dataframe: pd.DataFrame, measure: str, column_group: str, xlabel: 
     plt.tight_layout()
     plt.show()
 
-def k_distance(dataframe: pd.DataFrame, zoom_last_n_points: int = 20):
+def k_distance(data: np.array, zoom_last_n_points: int = 20):
 
     # n_neighbors/min_points based rule of thumb
-    n_neighbors = 2*len(dataframe.columns)
+    n_neighbors = 2*data.shape[1]
     
     # Plot k-distance graph
-    distances = compute_k_distances(data=dataframe.astype('float32'), n_neighbors=n_neighbors)
+    distances = compute_k_distances(data=data.astype('float32'), n_neighbors=n_neighbors)
     plt.plot(range(len(distances)), distances, marker='o', linestyle='-')
 
     # Set x-axis limits and ticks
-    x_min = dataframe.shape[0] - zoom_last_n_points
-    x_max = dataframe.shape[0] + 1
+    x_min = data.shape[0] - zoom_last_n_points
+    x_max = data.shape[0] + 1
     x_mid = (x_min + x_max) // 2
 
     plt.xlim([x_min, x_max])  # Correct method
