1c7c8be679ae21322af17ef39aced271113452a8
6oncvlo <gonprt06@gmail.com>
2024-12-04 18:15:57 +0000

    predictions_correction (rule-based)

---
+++ src/models/utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def predictions_correction(dataframe: pd.DataFrame):
+
+    # anomalous points with positive number of transactions should be non-anomalous
+    dataframe.loc[
+        (dataframe['anomaly_label']==1)
+        & (dataframe['num_transactions']>0)
+        , 'anomaly_label'] = 0
+    
+    return dataframe
