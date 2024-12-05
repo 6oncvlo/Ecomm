b160c510aeba9a87d6f2ebbb212f8d9c97a4dd32
6oncvlo <gonprt06@gmail.com>
2024-12-05 19:40:52 +0000

    log mlflow artifacts

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import mlflow
+import os
 
 def predictions_correction(dataframe: pd.DataFrame):
 
@@ -9,3 +11,13 @@ def predictions_correction(dataframe: pd.DataFrame):
         , 'anomaly_label'] = 1
     
     return dataframe
+
+def log_artifact(artifact_path: str, df: pd.DataFrame =None, df_name: str = None, image_name : str = None):
+    """Save dataframe or image, log it as an artifact, and then delete the file."""
+    if df is not None:
+        df.to_csv(f'{df_name}.csv')
+        mlflow.log_artifact(f'{df_name}.csv', artifact_path=artifact_path)
+        os.remove(f'{df_name}.csv')
+    elif image_name is not None:
+        mlflow.log_artifact(f'{image_name}.png', artifact_path=artifact_path)
+        os.remove(f'{image_name}.png')
