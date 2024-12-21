5eb9e77a8a287cc896a25191aec6ad56787dca18
6oncvlo <goncaloarv@gmail.com>
2024-12-21 16:25:30 +0000

    load sql

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,17 +1,16 @@
 import pandas as pd
+import pandasql as psql
 from src.data.utils import downcast_cols
 
-
-def load_data(data_paths: dict):
+def load_data(data_paths: dict)->dict:
 
     data = {
         table_name: pd.read_csv(table_path) for table_name, table_path in data_paths.items()
     }
     return data
 
-def prepare_data(data: dict, config: dict):
+def prepare_data(data: dict, config: dict)->dict:
 
-    # optimize events dataset
     # convert timestamp to Unix format and event type to a category
     data['events']['timestamp'] = pd.to_datetime(data['events']['timestamp'], unit='ms')
     data['events']['event'] = data['events']['event'].astype('category')
@@ -25,3 +24,14 @@ def prepare_data(data: dict, config: dict):
     data['events'] = downcast_cols(dataframe = data['events'])
 
     return data
+
+def load_queries(data_paths: dict, data: dict)->dict:
+    data_new = {}
+    
+    # process each table and its corresponding SQL query file path
+    for table_name, path in data_paths.items():
+        with open(path, 'r') as file:
+            query = file.read()
+        data_new[table_name] = downcast_cols(dataframe = psql.sqldf(query, data))
+        
+    return data_new
